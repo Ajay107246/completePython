5e9226cddbe6af2fe436c0bf0641c2b20daba084
Ajay Shimpi <shimpi.ajay.24@gmail.com>
2025-12-17 22:14:19 +0100

    python updated task1

--- task1.py
+++ task1.py
@@ -1,2 +1,12 @@
 # Write your code below this line
 print("Hello, World!")  # print simple string on console
+print("\nHello, World!\n\nHello, World!")  # new line in string
+print("Hello, Concatinate" + "Hello, World!")  # concatinate two string
+print("Hello, Concatinate" + " " + "Hello, World with space!")  # concatinate two
+# print("space before print - error: Unexpected indentation Pylance")
+print ("With space after print!")  # space after print
+print("   Hello, World with space before print!")  # space before print
+print("Hello, World with space after print!   .")  # space after print
+
+# print direct Input from user
+print("Hello " + input("Enter your input: ")) #output> Hello <user input>
